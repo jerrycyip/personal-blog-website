612fad1197b74265e3252e3076729c3cac16edcc
Jerry Yip <jerry.yip@gmail.com>
2020-09-01 14:59:52 -0700

    feat: enhanced responsive blog cards

--- js/app.js
+++ js/app.js
@@ -19,10 +19,15 @@
 */
 // Retrieve navigation bar menu for population later
 const navbar__list = document.querySelector('#navbar__list');
+// Retrieve navigation side bar menu for population later
+const sidebar_menu = document.querySelector('.sidebar');
 // Retrieve the NodeList of sections
 const sections = document.querySelectorAll('section.content-section');
-// ← to minimize reflow instances, using a DocumentFragment instead of a <div> to add html
-const fragment = document.createDocumentFragment();
+// ← to minimize reflow instances, use a DocumentFragment to add top Menu
+const fragmentTopMenu = document.createDocumentFragment();
+// ← to minimize reflow instances, use a DocumentFragment to add side Menu
+const fragmentSideMenu = document.createDocumentFragment();
+
 
 /**
  * End Global Variables
@@ -30,6 +35,21 @@ const fragment = document.createDocumentFragment();
  *
 */
 
+// helper function to identify if section is near top/center of viewport
+function isInView(elem) {
+    // Retrieve the element's size and position relative to viewport
+    const position = elem.getBoundingClientRect();
+    // compare position against viewport and window boundaries
+    // if within boundaries, return True, else False
+    return (
+        position.top >= 0 &&
+        position.left >= 0 &&
+        position.bottom  <= (window.innerHeight ||
+            document.documentElement.clientHeight) &&
+        position.right <= (window.innerWidth ||
+            document.documentElement.clientWidth)
+        );
+    }
 
 /**
  * End Helper Functions
@@ -37,53 +57,118 @@ const fragment = document.createDocumentFragment();
  *
 */
 
-// build the nav
-
-function buildNavMenu(){
-    // First add the 'about me/author info' section as that is a custom sidebar toggle rather than a main section
+// build the top nav menu
+function buildTopNavMenu(){
+    // First add the 'about me/author info' section
+    // 'About' section is a custom sidebar toggle rather than a main section
     const newLi = document.createElement('li');
     newLi.innerHTML = '<a href=#author-info>About</a>';
-    fragment.appendChild(newLi);
-    // Now, iterate over the main section to get it's title and anchor reference to add them to the navbar__menu
+    fragmentTopMenu.appendChild(newLi);
+    // Now, iterate over the main sections to get title and anchor references
+    // Use references when constructing the navbar__menu
     for(let section of sections) {
         // retrieve the custom data attribute 'data-nav'
         let sectionTitle = section.dataset.nav;
         // retrieve the id for scrolling functionality
         let sectionTarget = section.id;
-        // retrieve the anchor tag attribute for linking to
-//        let sectionAnchor = section.id
-        // create new <li> and <a> tags to hold contents
+        // create new <li> element
         const newLi = document.createElement('li');
-        /*const newA = document.createElement('a');*/
-        // create span to hold '|' decorative divider between menu items
+        // create span to hold decorative divider '|' between menu items
         const newSpan = document.createElement('span');
-        // define contents and attributes of new html elements within the <li>
         newSpan.innerHTML = '&nbsp|&nbsp';
-//        newA.setAttribute('href', '#' + sectionAnchor);
         // append new html elements to <li>
         newLi.appendChild(newSpan);
         newLi.insertAdjacentHTML('beforeend', sectionTitle);
+        // define custom data attribute of new <li> for scrolling functionality
         newLi.dataset.nav = sectionTarget;
-        /*newLi.appendChild(newA);*/
         //append new <li> entry to document fragment
-        fragment.appendChild(newLi);
+        fragmentTopMenu.appendChild(newLi);
     }
     // append new entries to navbar
-    navbar__list.appendChild(fragment);
+    navbar__list.appendChild(fragmentTopMenu);
 }
 
+// build the side navigation menu (accessed via dashboard button)
+function buildSideNavMenu(){
+    // First add the custom sections 'Home' and 'About Me'
+    const newLi1 = document.createElement('li');
+    newLi1.innerHTML = '<a href=#>Home</a><a href="#" class="material-icons sidebar-icons">home</a>';
+    fragmentSideMenu.appendChild(newLi1);
 
-
-// Add class 'active' to section when near top of viewport
-
+    // 'About' section is a custom sidebar toggle rather than a main section
+    const newLi2 = document.createElement('li');
+    newLi2.innerHTML = '<a href=#author-info>About</a><a href=#author-info class="material-icons sidebar-icons">portrait</a></li>';
+    fragmentSideMenu.appendChild(newLi2);
+    // Now, iterate over the main sections to get title and anchor references
+    // Use references when constructing the navbar__menu
+    for(let section of sections) {
+        // retrieve the custom data attribute 'data-nav'
+        let sectionTitle = section.dataset.nav;
+        // retrieve the id for scrolling functionality
+        let sectionTarget = section.id;
+        // create new <li> element
+        const newLi = document.createElement('li');
+        // create anchor to hold expansion icon
+        const newAnchor = document.createElement('a');
+        newAnchor.href = "#";
+        newAnchor.classList.add("material-icons", "sidebar-icons");
+        newAnchor.innerHTML = "expand_more";
+        // append new html elements to <li>
+        newLi.insertAdjacentHTML('afterbegin', sectionTitle);
+        newLi.appendChild(newAnchor);
+        // define custom data attribute of new <li> for scrolling functionality
+        newLi.dataset.nav = sectionTarget;
+        //append new <li> entry to document fragment
+        fragmentSideMenu.appendChild(newLi);
+    }
+    // Lastly add the sections 'Settings'
+        const newLiSettings = document.createElement('li');
+        newLiSettings.innerHTML = '<a href=#>Settings</a><a href="#" class="material-icons sidebar-icons">settings</a>';
+        fragmentSideMenu.appendChild(newLiSettings);
+    // append new entries to navbar
+    sidebar_menu.appendChild(fragmentSideMenu);
+}
 
 // Scroll to anchor ID using scrollIntoView event
 function scrollOnClick() {
     navbar__list.addEventListener('click', function (event) {
-        const target = document.querySelector('#' + event.target.dataset.nav)
-        target.scrollIntoView({
-      behavior: 'smooth'});
+        const scrollTarget = document.querySelector('#' +
+        event.target.dataset.nav)
+        scrollTarget.scrollIntoView({behavior: 'smooth'});
     })
+    sidebar_menu.addEventListener('click', function (event) {
+        const scrollTarget = document.querySelector('#' +
+        event.target.dataset.nav)
+        scrollTarget.scrollIntoView({behavior:'smooth'});
+    })
+}
+
+// Event Listener to update classList of active and inactive section (titles)
+function setActiveClass() {
+    window.addEventListener('scroll', function() {
+        // loop through sections to determine which is near top of viewport
+        for (let section of sections) {
+            // retrieve associated section title for modifying associated class
+            let sectionTitle = section.querySelector(".content-title");
+            let posts  = section.querySelectorAll(".blog-post");
+            if (isInView(section)) {
+                // add active class to active section's title
+                sectionTitle.classList.add("active-class");
+                // add active class to active section's blog cards
+                for (let post of posts) {
+                        post.classList.add("blog-post-active");
+                }
+            }
+            else {
+            // remove active class from all other sections
+                sectionTitle.classList.remove("active-class");
+                // add active class to active section's blog cards
+                for (let post of posts) {
+                        post.classList.remove("blog-post-active");
+                    }
+            }
+        }
+    });
 }
 
 /**
@@ -92,8 +177,10 @@ function scrollOnClick() {
  *
 */
 
-// Build menu
-buildNavMenu();
+// Build menus
+buildTopNavMenu();
+buildSideNavMenu();
 // Scroll to section on link click
 scrollOnClick();
 // Set sections as active
+setActiveClass();
